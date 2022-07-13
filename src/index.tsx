6c2551e98d95cf1c55d0c932c2180a5d531783a7
Florian Rappl <florian.rappl@smapiot.com>
2022-07-13 11:14:46 +0200

    Fixed tailwind build

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import 'piral/polyfills';
 import { renderInstance } from 'piral';
-import { layout, errors } from './layout';
+import { layout } from './layout';
 
 // change to your feed URL here (either using feed.piral.cloud or your own service)
 const feedUrl = 'https://feed.piral.cloud/api/v1/pilet/empty';
 
 renderInstance({
   layout,
-  errors,
   requestPilets() {
     return fetch(feedUrl)
       .then(res => res.json())
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const extendConfig = require("piral-cli-webpack5/extend-config");
 
-module.exports ==
-  extendConfig({
-    cssLoaderOptions: {
-      rules: {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: ["postcss-loader"],
-      },
+function getStyleLoaders() {
+  if (process.env.NODE_ENV === "production") {
+    return [MiniCssExtractPlugin.loader];
+  } else {
+    return ["style-loader"];
+  }
+}
+
+module.exports = extendConfig({
+  removeRules: ["css-loader"],
+  rules: [
+    {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: [...getStyleLoaders(), "css-loader", "postcss-loader"],
     },
-  });
+  ],
+});
